8fd782b5d5ae8f927b6840f022ccbd4aa9ce9335
nicholasdaniel <nicholasjdaniel@gmail.com>
2018-09-26 15:07:47 -0400

    add auth endpoint

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/auth", Auth).Methods("PUt")
+
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+// User details for authenticating
+type User struct {
+	UUID     string `json:"uuid,omitempty"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Auth controller(handler?) to authenticate user with
+// username/password and return token
+func Auth(w http.ResponseWriter, r *http.Request) {
+	admin := User{UUID: "1", Username: "admin", Password: "pass"}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		log.Print(err)
+	}
+
+	// Validate user/passwd
+	if user.Username == admin.Username && user.Password == admin.Password {
+		// Create Token
+		fmt.Println("Match!")
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+}
