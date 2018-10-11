66ec1b0273e639dd3e81e4cd38b3d30b32f562b4
nick <nicholasjdaniel@gmail.com>
2018-10-11 13:24:51 -0400

    Add product

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	web "github.com/njdaniel/jwt-go-example"
@@ -126,4 +127,14 @@ func TestGetProduct(t *testing.T) {
 	response := executeRequest(req)
 
 	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
+	}
 }
